5539dd3f64b6510e1cd2b50684cdc819c34278de
BV Satyaram <bvsatyaram@gmail.com>
2015-02-28 15:47:51 +0530

    Loading tasks from json file

--- js/app.js
+++ js/app.js
@@ -1,16 +1,7 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('ListController', ['$scope', function($scope) {
-  $scope.tasks = [
-    "Learn Angular",
-    "Show Off",
-    "Get Job",
-    "Build an App",
-    "Go to Goa",
-    "Explore WWW",
-    "Buy a car",
-    "Get married",
-    "Have kids",
-    "Enjoy"
-  ];
+myApp.controller('ListController', ['$scope', '$http', function($scope, $http) {
+  $http.get('js/tasks.json').success(function(data) {
+    $scope.tasks = data;
+  });
 }]);
